6d69f068e74bdb75537545991dd3609f9a92f83c
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-06-06 23:14:25 -0500

    changes

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,15 @@
 // variables
 
 let deck;
-let hitYes= true;
+let hitYes= true;       //need to draw when less than 21
 
-let dealerSum = 0;
+let dealerSum = 0;      //need to keep track of cards
 let playerSum = 0;
-let dealerAces = 0;
-let playerAces = 0;
-let facedown;
+let dealerAces = 0;     //need to keep track of Aces
+let playerAces = 0;    
+let facedown;           //one crd facedown
 
-window.onload = function(){
+window.onload = function(){     //to start once window open
     makeDeck();
     shuffleDeck();
     getStarted();
@@ -36,55 +36,55 @@ function shuffleDeck(){
 }
 
 function getStarted(){
-    facedown = deck.pop();
+    facedown = deck.pop();      //takes one card to be facedown
     dealerSum += cardValue(facedown);
     dealerAces += isAce(facedown);
-    while (dealerSum < 17 ){ 
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";          
-        dealerSum += cardValue(card);
-        dealerAces += isAce(card);
-        document.getElementById("dealerCards").append(cardImg);
+    while (dealerSum < 17 ){                             //keeps dealers card less than 17 on initial
+        let cardImg = document.createElement("img");    //creates crd img
+        let card = deck.pop();                          //one crd from deck
+        cardImg.src = "./cards/" + card + ".png";      //adds image to value by src   
+        dealerSum += cardValue(card);                   //add value of crd
+        dealerAces += isAce(card);                      //ck ace add to total
+        document.getElementById("dealerCards").append(cardImg); 
         }
-    for (let i=0; i<2; i++){
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";
-        playerSum += cardValue(card);
-        playerAces += isAce(card);
-        document.getElementById("playerCards").append(cardImg);
+    for (let i=0; i<2; i++){        //allows 2 cards to start
+        let cardImg = document.createElement("img");    
+        let card = deck.pop();              //pull a card from deck
+        cardImg.src = "./cards/" + card + ".png";  //how to put images w/values
+        playerSum += cardValue(card);     //adds to sum with each card
+        playerAces += isAce(card);      //checks for Aces
+        document.getElementById("playerCards").append(cardImg); //add image to player cards
     }
-    document.getElementById("hit").addEventListener("click", hit);
-    document.getElementById("stay").addEventListener("click", stay);
+    document.getElementById("hit").addEventListener("click", hit);  //when clicked get new card
+    document.getElementById("stay").addEventListener("click", stay);    //stop game and add score
 }
 
 function hit(){
     if (!hitYes){
         return;
     } 
-        let cardImg = document.createElement("img");
+        let cardImg = document.createElement("img");    //get image for new crd
         let card = deck.pop();
-        cardImg.src = "./cards/" + card + ".png";
-        playerSum += cardValue(card);
-        playerAces += isAce(card);        
+        cardImg.src = "./cards/" + card + ".png";   //combines value and img
+        playerSum += cardValue(card);               //add value of new crd
+        playerAces += isAce(card);                  //add value of Ace
         document.getElementById("playerCards").append(cardImg);
-    if(reduceAce(playerSum, playerAces) > 21){
-            hitYes = false;
+    if(reduceAce(playerSum, playerAces) > 21){  //goal to not go over 21
+            hitYes = false;         //stops new cards from coming
     }
 }
 
 
 function cardValue(card){
-    let data = card.split("-");
+    let data = card.split("-");     //slits card so value is separated out
     let value = data[0];
-    if (isNaN(value)){
-        if(value == "A"){
+    if (isNaN(value)){          //ck for KQJA
+        if(value == "A"){       //deals with A's before hitting 21
         return 11;
         }
-        return 10;
+        return 10;              //KQJ all equal 10
     }
-    return parseInt(value);
+    return parseInt(value);     //returns value of crd number since separated
 }
 function isAce(card){
     if(card[0] == "A"){
@@ -93,20 +93,20 @@ function isAce(card){
      return 0;
 }
 
-function reduceAce(playerSum, playerAces){
-    while(playerSum > 21 && playerAces > 0){
+function reduceAce(playerSum, playerAces){  //deals with A's being either 1 or 11
+    while(playerSum > 21 && playerAces > 0){        //conditions
         playerSum -= 10;
         playerAces -= 1;
     }
     return playerSum;
 }
 
-function stay(){
+function stay(){           //stay button clicked stops and totals amt
     dealerSum = reduceAce(dealerSum, dealerAces);
     playerSum = reduceAce(playerSum, playerAces);
     hitYes = false;
     document.getElementById("facedown").src = "./cards/" + facedown + ".png";
-    let message = "";
+    let message = "";               //gives multiple messages for diff totals
     if (playerSum > 21){
         message = "You Lose!";
     }
@@ -120,9 +120,9 @@ function stay(){
     }else if (playerSum < dealerSum){
         message = "You lose!";
     }
-    document.getElementById("dealerSum").innerText = dealerSum; 
-    document.getElementById("playerSum").innerText = playerSum;
-    document.getElementById("win").innerText = message;   
+    document.getElementById("dealerSum").innerText = dealerSum; //reveals final amt.
+    document.getElementById("playerSum").innerText = playerSum; //reveals final amt.
+    document.getElementById("win").innerText = message;   //tells who won at top
     }
 
     
